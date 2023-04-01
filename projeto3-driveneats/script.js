6fbd95060666c71c3e5fbbdef4b2ab9e8aaac30b
GabrielBragaa <gbragamoura@hotmail.com>
2023-04-01 01:05:43 -0300

    tela de confirmaçao e redirecionamento implementados

--- projeto3-driveneats/script.js
+++ projeto3-driveneats/script.js
@@ -1,6 +1,19 @@
 let prato, precoPrato, bebida, precoBebida, sobremesa, precoSobremesa, precoTotal;
 
 
+function verificaSelecao() {
+    if (prato !== undefined) {
+        if (bebida !== undefined) {
+            if (sobremesa !== undefined) {
+                const botaoPedido = document.querySelector('.botao');
+                botaoPedido.innerHTML = 'Fechar pedido';
+                botaoPedido.classList.add('fechar-pedido');
+                botaoPedido.removeAttribute('disabled');
+            } 
+        }
+    }
+}
+
 function selecionarPrato(seletor) {
     const opcaoAtualPrato = seletor;
     const opcaoAnteriorPrato = document.querySelector('.comes .selecionado');
@@ -49,14 +62,30 @@ function selecionarSobremesa(seletor) {
     verificaSelecao();
 }
 
-function verificaSelecao() {
-    if (prato !== undefined) {
-        if (bebida !== undefined) {
-            if (sobremesa !== undefined) {
-                const botaoPedido = document.querySelector('.botao');
-                botaoPedido.innerHTML = 'Fechar pedido';
-                botaoPedido.classList.add('fechar-pedido');
-            } 
-        }
-    }
+function confirmar() {
+    const tela = document.querySelector('.confirmacao');
+    tela.classList.remove('escondido');
+    tela.querySelector('.nome-prato-confirmado').innerHTML = prato;
+    tela.querySelector('.preco-prato-confirmado').innerHTML = precoPrato;
+    tela.querySelector('.nome-bebida-confirmada').innerHTML = bebida;
+    tela.querySelector('.preco-bebida-confirmada').innerHTML = precoBebida;
+    tela.querySelector('.nome-sobremesa-confirmada').innerHTML = sobremesa;
+    tela.querySelector('.preco-sobremesa-confirmada').innerHTML = precoSobremesa;
+}
+
+function cancelar() {
+    const tela = document.querySelector('.confirmacao');
+    tela.classList.add('escondido');
+}
+
+function redirecionar() {
+    let nome = prompt('Digite o seu nome: ');
+    let endereco = prompt('Digite o seu endereço: ');
+    
+    let msg = `Olá, gostaria de fazer o pedido:\n - Prato:${prato} - Bebida: ${bebida} \n - Sobremesa: ${sobremesa} \n Total: R$ 27,70 \n\n Nome: ${nome} \n Endereço: ${endereco}`;
+
+    let num = "55021981199598"
+
+    msg = window.encodeURIComponent(msg);
+    window.open("https://wa.me/" + num + "?text=" + msg);
 }
